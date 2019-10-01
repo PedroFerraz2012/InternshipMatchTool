a08b82aff5a84884914afcc3bb8f3d7449233336
PedroFerraz2012 <pedroferraz2012@gmail.com>
2019-10-02 09:17:05 +1000

    emails, pineapple css js fixed

--- Emails.php
+++ Emails.php
@@ -4,29 +4,33 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<!--head>
+  OK <title>Internship Match Tool</title>
+  OK <meta charset="utf-8">
+  OK <meta name="viewport" content="width=device-width, initial-scale=1">
+ OK <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link rel="shortcut icon" href="AIT_icon.ico" type="image/x-icon"/>
+  <link rel="stylesheet" type="text/css" href="Styles.css">
+</head-->
+
     <head>
         <title>Internship Match Tool</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
-
-        <!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css-->
-        <!--desabling this-->
-        <!--script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>-->
         <link rel="shortcut icon" href="AIT_icon.ico" type="image/x-icon" />
-
-        <!--this is Jayme's file CHECKED NOT CONFLICTING-->
         <link rel="stylesheet" type="text/css" href="Styles.css">
-
-
-
 
         <!--FullCalendar css-->
         <link href='Calendar/css/core/main.min.css' rel='stylesheet' />
         <link href='Calendar/css/daygrid/main.min.css' rel='stylesheet' />
         <!-- css bootstrap for modal-->
         <!--desabling this for while-->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        
 
         <!-- my css for calendar: custom.css CHECKED NOT CONFLICTING-->
         <link rel="stylesheet" href="Calendar/css/custom.css">
@@ -36,30 +40,32 @@
         <script src='./Calendar/js/daygrid/main.min.js'></script>
         <script src='./Calendar/js/core/locales/en-au.js'></script>
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 
         <!-- js bootstrap for modal-->
+        <!--script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script-->
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
         <script src="./Calendar/js/custom.js"></script>
-        
-        
+
+
     </head>
-<body>
-    <?php include('navigation.php'); ?>
 
-    <div class="container-fluid text-center">
-        <div class="row content">
-            <div class="col-sm-2 sidenav">
-                <a class="text">
-                    <div class="well1">
-                        <p class="t2">Communication</p>
-                    </div>
-                </a>
+    <body>
+        <?php include('navigation.php'); ?>
 
-            </div>
-            <div class="col-sm-10 text-left content">
-            <?php
+        <div class="container-fluid text-center">
+            <div class="row content">
+                <div class="col-sm-2 sidenav">
+                    <a class="text">
+                        <div class="well1">
+                            <p class="t2">Communication</p>
+                        </div>
+                    </a>
+
+                </div>
+                <div class="col-sm-10 text-left content">
+                    <?php
         
         if(isset($_SESSION['msg']))
     {
@@ -70,129 +76,134 @@
     header( "refresh:4;url=http://localhost/_myFiles/internshipMatchTool/Emails.php" );
     }
         ?>
-                <!--where the calendar comes-->                
-                <div id='calendar' margin-top='0'></div>
+                    <!--where the calendar comes-->
+                    <div id='calendar' margin-top='0'></div>
+                </div>
             </div>
         </div>
-    </div>
 
-    <footer id="sticky-footer" class="footer12">
-    <div class="container text-center">
-     <P>By Pedro Ferraz 6008 and Jayme Schmid 6290</P>
-    </div>
-  </footer>
-
-    <!--MODALS-->
-    
-    <!-- Modal View details-->
-<div class="modal fade" id="view" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Event Details</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+        <footer id="sticky-footer" class="footer12">
+            <div class="container text-center">
+                <P>By Pedro Ferraz 6008 and Jayme Schmid 6290</P>
             </div>
+        </footer>
 
-            <div class="modal-body">
-                <dl class="row">
+        <!--MODALS-->
 
-                    <dt class="col-sm-3">Event Title: </dt>
-                    <dd class="col-sm-9" id="title"></dd>
-
-                    <dt class="col-sm-3">To: </dt>
-                    <dd class="col-sm-9" id="to_whom"></dd>
-
-                    <dt class="col-sm-3">Date: </dt>
-                    <dd class="col-sm-9" id="start"></dd>
-
-                    <dt class="col-sm-3">Content: </dt>
-                    <dd class="col-sm-9" id="content"></dd>
-
-                    <dt class="col-sm-3">Status</dt>
-                    <dd class="col-sm-9" id="status"></dd>
-                </dl>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Modal add -->
-
-<div class="modal fade" id="cadastrar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Add Event</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <span id="msg-cad"></span>
-                <form id="addevent" method="POST" enctype="multipart/form-data">
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Title</label>
-                        <div class="col-sm-10">
-                            <input type="text" name="title" class="form-control" id="title" placeholder="Event Title">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">To</label>
-                        <div class="col-sm-10">
-                            <input type="text" name="to_whom" class="form-control" id="to_whom" placeholder="to whom?">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Color</label>
-                        <div class="col-sm-10">
-                            <select name="color" class="form-control" id="color">
-                                <option value="">Select Colour</option>
-                                <option style="background:#FFD700;" value="#FFD700">Yellow</option>
-                                <option style="background:#0071c5;" value="#0071c5">Blue</option>
-                                <option style="background:#FF4500;" value="#FF4500">Orange</option>
-                                <option style="background:#A020F0;" value="#A020F0">Purple</option>
-                                <option style="background:#228B22;" value="#228B22">Green</option>
-                                <option style="background:#8B0000;" value="#8B0000">Red</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Date: </label>
-                        <div class="col-sm-10">
-                            <input type="text" name="start" class="form-control" id="start"
-                                onkeypress="DataHora(event, this)">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Remind in </label>
-                        <div class="col-sm-10">
-                            <input type="text" name="remind_date" class="form-control" id="remind_date"
-                                placeholder="(days)">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Content </label>
-                        <div class="col-sm-10">
-                            <input type="text" name="content" class="form-control" id="content" placeholder="Message">
-                        </div>
+        <!-- Modal View details-->
+        <div class="modal fade" id="view" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Event Details</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
 
-                    <div class="form-group row">
-                        <div class="col-sm-10">
-                            <button type="submit" name="CadEvent" id="CadEvent" value="CadEvent"
-                                class="btn btn-success">Add</button>
-                        </div>
+                    <div class="modal-body">
+                        <dl class="row">
+
+                            <dt class="col-sm-3">Event Title: </dt>
+                            <dd class="col-sm-9" id="title"></dd>
+
+                            <dt class="col-sm-3">To: </dt>
+                            <dd class="col-sm-9" id="to_whom"></dd>
+
+                            <dt class="col-sm-3">Date: </dt>
+                            <dd class="col-sm-9" id="start"></dd>
+
+                            <dt class="col-sm-3">Content: </dt>
+                            <dd class="col-sm-9" id="content"></dd>
+
+                            <dt class="col-sm-3">Status</dt>
+                            <dd class="col-sm-9" id="status"></dd>
+                        </dl>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
-    </div>
-</div>
+
+        <!-- Modal add -->
+
+        <div class="modal fade" id="cadastrar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Add Event</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <span id="msg-cad"></span>
+                        <form id="addevent" method="POST" enctype="multipart/form-data">
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Title</label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="title" class="form-control" id="title"
+                                        placeholder="Event Title">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">To</label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="to_whom" class="form-control" id="to_whom"
+                                        placeholder="to whom?">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Color</label>
+                                <div class="col-sm-10">
+                                    <select name="color" class="form-control" id="color">
+                                        <option value="">Select Colour</option>
+                                        <option style="background:#FFD700;" value="#FFD700">Yellow</option>
+                                        <option style="background:#0071c5;" value="#0071c5">Blue</option>
+                                        <option style="background:#FF4500;" value="#FF4500">Orange</option>
+                                        <option style="background:#A020F0;" value="#A020F0">Purple</option>
+                                        <option style="background:#228B22;" value="#228B22">Green</option>
+                                        <option style="background:#8B0000;" value="#8B0000">Red</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Date: </label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="start" class="form-control" id="start"
+                                        onkeypress="DataHora(event, this)">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Remind in </label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="remind_date" class="form-control" id="remind_date"
+                                        placeholder="(days)">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Content </label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="content" class="form-control" id="content"
+                                        placeholder="Message">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <div class="col-sm-10">
+                                    <button type="submit" name="CadEvent" id="CadEvent" value="CadEvent"
+                                        class="btn btn-success">Add</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
 
     </body>
+
 </html>
--- navigation.php
+++ navigation.php
@@ -1,103 +1,80 @@
+<div class="jumbotron">
+    <div class="container-fluid text-center">
 
-      <div class="jumbotron">
-            <div class="container-fluid text-center">
-             
-                
-                <div class="row">
-                     <div class="col-sm-4" ></div>
-                    <div class="col-sm-4"  class="headingTxt"><h2>AIT Internship Match Tool</h2></div>
-      <div class="col-sm-4" > <a href="pineapple.php">
-                  <h4 class="h4txt"> Powered by <b>Pineapple</b> Solutions ©
-                    <img class="img14" src="./images/Pineapple_Solutions_logo.svg" alt="PineapleSolutiosLogo">
-                  </h4></a></div>
-    </div>
-                
-              
-              </div>
+
+        <div class="row">
+            <div class="col-sm-4"></div>
+            <div class="col-sm-4" class="headingTxt">
+                <h2>AIT Internship Match Tool</h2>
             </div>
-    
-    
-    
-    
-    
-    
-    <nav class="navbar navbar-expand-sm navicolor " id="myNavbar">
-  <a class="navbar-brand" href="Home.php"><img class="img13" src="./images/AIT_logo.svg" alt="aitlogo"></a>
-  <ul class="navbar-nav li18">
-    <li class="nav-item li12">
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-      <a class="nav-link" style="color:white;"  href="Companies.php">
-        <div class="svgfilter">
-        <svg class="svgpos" width="50" height="50">
-        <?php echo file_get_contents("./images/university-solid.svg"); ?>
-        </svg>
+            <div class="col-sm-4"> <a href="pineapple.php">
+                    <h4 class="h4txt"> Powered by <b>Pineapple</b> Solutions ©
+                        <img class="img14" src="./images/Pineapple_Solutions_logo.svg" alt="PineapleSolutiosLogo">
+                    </h4>
+                </a></div>
         </div>
-        <div class="page-link-text">Companies</div>    
-        </a>
-    </li>
-      
-       <li class="nav-item li12">
-      <a class="nav-link" style="color:white;" href="Students.php">
-       <div class="svgfilter">
-        <svg class="svgpos" width="50" height="50">
-        <?php echo file_get_contents("./images/user-graduate-solid.svg"); ?>
-         </svg>
-        </div>
-        <div class="page-link-text">Students</div>    
-        </a>
-    </li>
-      
-       <li class="nav-item li12">
-      <a class="nav-link" style="color:white;" href="Enrollments.php">
-        <div class="svgfilter">
-        <svg class="svgpos" width="50" height="50">
-        <?php echo file_get_contents("./images/hat-wizard-solid.svg"); ?>
-        </svg>
-        </div>
-        <div class="page-link-text">Teck Stack/Assets</div>   
-        </a>
-    </li>
-      
-       <li class="nav-item li12">
-      <a class="nav-link"  style="color:white;" href="StudentMatch.php">
-          <div class="svgfilter">
-         <svg class="svgpos" width="50" height="50">
-         <?php echo file_get_contents("./images/match_tool.svg"); ?>
-        </svg>
-        </div>
-        <div class="page-link-text">Match Tool</div>   
-        </a>
-    </li>
-       <li class="nav-item li12">
-      <a class="nav-link" style="color:white;" href="Emails.php">
-       <div class="svgfilter">
-       <svg class="svgpos" width="50" height="50">
-       <path class="sv12" />
-        <?php echo file_get_contents("./images/comment-dots-solid.svg"); ?>
-        </svg>
-        </div>
-        <div class="page-link-text">Communication</div>   
-        </a>
-    </li>
-      
-      
-      
-    
-  </ul>
+
+
+    </div>
+</div>
+
+<nav class="navbar navbar-expand-sm navicolor " id="myNavbar">
+    <a class="navbar-brand" href="Home.php"><img class="img13" src="./images/AIT_logo.svg" alt="aitlogo"></a>
+    <ul class="navbar-nav li18">
+        <li class="nav-item li12">
+
+            <a class="nav-link" style="color:white;" href="Companies.php">
+                <div class="svgfilter">
+                    <svg class="svgpos" width="50" height="50">
+                        <?php echo file_get_contents("./images/university-solid.svg"); ?>
+                    </svg>
+                </div>
+                <div class="page-link-text">Companies</div>
+            </a>
+        </li>
+
+        <li class="nav-item li12">
+            <a class="nav-link" style="color:white;" href="Students.php">
+                <div class="svgfilter">
+                    <svg class="svgpos" width="50" height="50">
+                        <?php echo file_get_contents("./images/user-graduate-solid.svg"); ?>
+                    </svg>
+                </div>
+                <div class="page-link-text">Students</div>
+            </a>
+        </li>
+
+        <li class="nav-item li12">
+            <a class="nav-link" style="color:white;" href="Enrollments.php">
+                <div class="svgfilter">
+                    <svg class="svgpos" width="50" height="50">
+                        <?php echo file_get_contents("./images/hat-wizard-solid.svg"); ?>
+                    </svg>
+                </div>
+                <div class="page-link-text">Teck Stack/Assets</div>
+            </a>
+        </li>
+
+        <li class="nav-item li12">
+            <a class="nav-link" style="color:white;" href="StudentMatch.php">
+                <div class="svgfilter">
+                    <svg class="svgpos" width="50" height="50">
+                        <?php echo file_get_contents("./images/match_tool.svg"); ?>
+                    </svg>
+                </div>
+                <div class="page-link-text">Match Tool</div>
+            </a>
+        </li>
+        <li class="nav-item li12">
+            <a class="nav-link" style="color:white;" href="Emails.php">
+                <div class="svgfilter">
+                    <svg class="svgpos" width="50" height="50">
+                        <path class="sv12" />
+                        <?php echo file_get_contents("./images/comment-dots-solid.svg"); ?>
+                    </svg>
+                </div>
+                <div class="page-link-text">Communication</div>
+            </a>
+        </li>
+    </ul>
 </nav>
-    
