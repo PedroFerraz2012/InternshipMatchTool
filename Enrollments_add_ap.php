24b86b222898a9ef3fa86619cb3f8abfe6290db7
JaymeSchmid1998 <42628938+JaymeSchmid1998@users.noreply.github.com>
2019-10-03 22:30:05 +1000

    course tech stack added
    added functionality

---
+++ Enrollments_add_ap.php
@@ -0,0 +1,112 @@
+
+ <?php
+
+
+
+
+
+
+             
+
+
+
+
+session_start();
+    $servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "internshipdatabase";
+
+    
+  // Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+
+$test= false; 
+
+    
+    
+    if( $test==false){
+        echo "flase";
+   
+    
+    $newTechStack= $_POST["NewTechS"];    
+
+    
+    
+    
+ 
+    
+
+   
+        
+       
+        
+    
+           try{ $sql = "ALTER TABLE student_assets
+ADD ".$newTechStack." int;
+   ";
+    // use exec() because no results are returned
+    
+               
+    
+if ($conn->query($sql) === TRUE) {
+    echo "updated";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}  
+               
+               
+               
+               
+               
+               
+ echo "updated";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+     $_SESSION["messageReg12"] ="added";
+            echo $_SESSION["messageReg12"];    
+}
+
+$conn->close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?> 
