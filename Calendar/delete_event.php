ac811d29a8f793777218e7bd09e44bc0af86d886
PedroFerraz2012 <43510971+PedroFerraz2012@users.noreply.github.com>
2019-10-12 17:03:35 +1100

    update delete_event

--- Calendar/delete_event.php
+++ Calendar/delete_event.php
@@ -11,16 +11,20 @@ $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
 //preparing query to save in the db, using links for values
 
 if (!empty($id)) {
-    $result_events = "DELETE FROM messages WHERE message_id ='$id'";
-    $resultado_events = mysqli_query($conn2, $result_events);
-    
+    //$result_events = "DELETE FROM messages WHERE message_id in ($id)";
+    $result_events = "DELETE FROM messages WHERE message_id = $id"; // myphpadmin is asking confirmation
+    $resultado_events = mysqli_query($conn2, $result_events, true);
+
+    //$update_event = $conn2->prepare($query_event);
+        //getting values to link in the variable
+    //$update_event->bindParam(':id', $dados['id']);
     
     //checking if DB changed by "mysqli_affected_rows"
     if (mysqli_affected_rows($conn2)) {
         $_SESSION['msg'] = "<div class='alert alert-success' role='alert'>Dropped Successfuly<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
         header("Location: ../Emails.php");
     } else {
-        $_SESSION['msg'] = "<div class='alert alert-danger' role='alert'>Error at Dropping Event <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
+        $_SESSION['msg'] = "<div class='alert alert-danger' role='alert'>Error at Dropping Event $id<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
         header("Location: ../Emails.php");
     }
 } else {
