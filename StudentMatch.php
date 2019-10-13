2fa09e3d189cef0b2605fef864f6c49a14038514
jayme schmid <jayme.schmid@gmail.com>
2019-10-12 19:14:45 -0700

    updated and matches students to companies

--- StudentMatch.php
+++ StudentMatch.php
@@ -1,45 +1,222 @@
+<?php
+if (session_id() == '' || !isset($_SESSION)) {
+
+session_start();
+   
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+$dbname = "internshipdatabase";
+    try{
+    $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+    $test=false;
+        $numberOfItems1= 0;
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT s_id,first_name,last_name,dob,email,notes,student_id FROM students";
+$result = $conn->query($sql);
+    
+        
+}
+    catch(PDOException $e){
+    echo $sql . "<br>" . $e->getMessage(); 
+}
+}
+ else {
+    echo "0 results";
+}
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>Internship Match Tool</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <link rel="shortcut icon" href="AIT_icon.ico" type="image/x-icon"/>
-  <link rel="stylesheet" type="text/css" href="Styles.css">
-</head>
-<body>
 
-<?php include('navigation.php'); ?>
-  
-<div class="container-fluid text-center">    
-  <div class="row content">
-    <div class="col-sm-2 sidenav">
-      <a class="text">
-        <div class="well1">
-     <p class="t2">Match Tool</p>
-      </div>
-       </a> 
-        
-    </div>
-    <div class="col-sm-10 text-left content"> 
-      <h1>Welcome</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <hr>
-      <h3>Test</h3>
-      <p>Lorem ipsum...</p>
-    </div>
+    <head>
+        <title>Internship Match Tool</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <link rel="shortcut icon" href="AIT_icon.ico" type="image/x-icon" />
+        <link rel="stylesheet" type="text/css" href="Styles.css">
+    </head>
+
+
+    <?php include('navigation.php'); ?>
+
+    <div class="container-fluid text-center">
+        <div class="row content">
+            <div class="col-sm-2 sidenav">
+                <a class="text">
+                    <div class="well1">
+                        <p class="t2">Student Match</p>
+                    </div>
+                </a>
+                <a class="text" href="Students_details.php">
+                    <div class="well2">
+                        <p class="t2">Companies Match </p>
+                    </div>
+                </a>
+
+                
+
+            </div>
+            <div class="col-sm-10 text-left content">
+
+                <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search by first name.."
+                    title="Type in a name">
+                <input type="text" id="myInput1" onkeyup="myFunction2()" placeholder="Search by last name.."
+                    title="Type in a name">
+
+
+
+
+                <table id="myTable" style="width:100%">
+                    <tr class="rowhead">
+                        <th class="colhead">Fist name</th>
+                        <th class="colhead">Last name</th>
+                        <th class="colhead">dob</th>
+                        <th class="colhead">Email</th>
+                        <th class="colhead">Notes</th>
+                        <th class="colhead">Student id</th>
+                        <th class="colhead">Course</th>
+                        <th class="colhead"></th>
+                    </tr>
+
+
+
+
+                    <?php 
     
-  </div>
-</div>
+    $sql = "SELECT s_id,first_name,last_name,dob,email,notes,student_id,courses FROM students";
+$result = $conn->query($sql);
+    while($row = $result->fetch_assoc()) {
+        
+            
+         ?>
 
-<footer id="sticky-footer" class="footer12">
-    <div class="container text-center">
-     <P>By Pedro Ferraz 6008 and Jayme Schmid 6290</P>
+
+
+
+                    <tr class="info-row">
+                        <td class="colhead1"><?php echo $row["first_name"];?>
+                        </td>
+                        <td class="colhead1">
+                            <?php echo $row["last_name"];?></td>
+                        <td class="colhead1">
+                            <?php echo $row["dob"];?></td>
+                        <td class="colhead1">
+                            <?php echo $row["email"];?></td>
+                        <td class="colhead1">
+                            <?php echo $row["notes"];?></td>
+                        <td class="colhead1">
+                            <?php echo $row["student_id"];?></td>
+                        <td class="colhead1">
+                            <?php echo $row["courses"];?></td>
+                        <td>
+                            <form action="StudentMatch_ap.php" method=post>
+                                <input type="hidden" name="stuId" value="<?php echo $row["s_id"];?>">
+
+
+                                <input class="infoBtn" type="submit"  alt="submit"
+                                    fill="orange" value="match them" width="17" height="17">
+
+                            </form>
+                        </td>
+
+                    </tr>
+
+
+
+
+                    <?php } ?>
+
+
+                </table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+
+        </div>
     </div>
-  </footer>
 
-</body>
+    <footer id="sticky-footer" class="footer12">
+        <div class="container text-center">
+            <P>By Pedro Ferraz 6008 and Jayme Schmid 6290</P>
+        </div>
+    </footer>
+
+    </body>
+
 </html>
+
+
+
+<script>
+function myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function myFunction2() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput1");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+</script>
---
+++ StudentMatch_Details.php
@@ -0,0 +1,347 @@
+
+<?php
+
+
+if (session_id() == '' || !isset($_SESSION)) {
+
+session_start();
+
+    $servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "internshipdatabase";
+    try{
+    $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+    $test=false;
+        $numberOfItems1= 0;
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT company_id,company_name,contact_person,website,description,tier_rate,notes FROM company";
+$result = $conn->query($sql);
+    
+     
+        
+}
+    catch(PDOException $e){
+    echo $sql . "<br>" . $e->getMessage(); 
+}
+}
+ else {
+    echo "0 results";
+}
+
+    
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Internship Match Tool</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link rel="shortcut icon" href="AIT_icon.ico" type="image/x-icon"/>
+  <link rel="stylesheet" type="text/css" href="Styles.css">
+</head>
+
+<?php include('navigation.php'); ?>
+  
+<div class="container-fluid text-center">    
+  <div class="row content">
+  <div class="col-sm-2 sidenav">
+                <a class="text">
+                    <div class="well1">
+                        <p class="t2">Student Match</p>
+                    </div>
+                </a>
+                <a class="text" href="Students_details.php">
+                    <div class="well2">
+                        <p class="t2">Companies Match </p>
+                    </div>
+                </a>
+
+                
+
+            </div>
+    <div class="col-sm-10 text-left content"> 
+        
+        
+        <?php 
+        
+           if(isset($_SESSION["stuId"])){
+        
+        ?>
+        
+        
+        
+      <table id="myTable" style="width:100%">
+  <tr class="rowhead">
+   <th class="colhead">first name</th>
+    
+    <th class="colhead">last name</th>
+     <th class="colhead">dob</th>    
+                <th class="colhead">email</th>
+                    <th class="colhead">notes</th>
+                 <th class="colhead">student id</th>
+      
+  </tr>
+  
+    
+    
+    
+     <?php 
+
+         
+          
+          
+          
+       
+          
+                $session12=$_SESSION["stuId"];
+    $sql = "SELECT s_id,first_name,last_name,dob,email,notes,student_id,courses FROM students";
+$result = $conn->query($sql);
+    while($row = $result->fetch_assoc()) {
+        if($row["s_id"]== $session12){
+           
+         ?>
+            
+            
+            
+            
+            <tr>
+           <td ><?php echo $row["first_name"];?>
+               </td>
+             <td>
+          <?php echo $row["last_name"];?></td>   
+                 <td>
+          <?php echo $row["dob"];?></td> 
+              <td>
+          <?php echo $row["email"];?></td>   
+        <td>
+          <?php echo $row["notes"];?></td>
+                 <td>
+          <?php echo $row["student_id"];?></td>
+        <?php
+                
+              $StuCourse = $row["courses"];  
+                
+                
+                
+                
+                ?>
+               
+            
+            </tr>
+            
+            
+            
+  
+        <?php }}?>
+    
+ 
+</table>
+        
+         <h2>Tech stack</h2>
+         <table id="myTable" style="width:100%">
+  <tr class="rowhead">
+   
+    
+    <th class="colhead">Tech skill</th>
+     <th class="colhead">Coding Ability</th>    
+                <th class="colhead">Social ability</th>
+      <th class="colhead">Punctuality_Reliability</th>
+        <th class="colhead">Team Dynamics</th>
+      <th class="colhead">Job Ready Professionalism</th>
+      <th class="colhead">Hardworking</th>
+         <th class="colhead">Combined score</th>
+  </tr>
+  
+    
+    
+    
+     <?php 
+ 
+       $session12=$_SESSION["stuId"];
+             
+    $sql = "SELECT * FROM student_assets";
+$result = $conn->query($sql);
+    while($row = $result->fetch_assoc()) {
+        if($row["student_id"]== $session12){
+           
+         ?>
+            
+            
+            
+            
+            <tr>
+                <form action="Students_details_edit_ap.php" method=post>
+           <td ><?php echo $row["tech_skill"];?>            
+               </td>
+                    <td ><?php echo $row["coding_ability"];?>            
+               </td>
+                
+                 <td ><?php echo $row["social_ability"];?> 
+                    
+               </td>
+                
+             <td><?php echo $row["punctuality_reliability"];?>
+          </td>   
+                 <td><?php echo $row["team_dynamics"];?>
+         </td> 
+                    
+          <td><?php echo $row["job_ready_professionalism"];?>
+          </td>
+                    
+                  <td><?php echo $row["hardworking"];?>
+          </td>
+               
+                    <?php  $combinedscore=$row["hardworking"]+$row["job_ready_professionalism"]+$row["team_dynamics"]+$row["punctuality_reliability"]+$row["social_ability"]+$row["coding_ability"]+$row["tech_skill"] ?>
+                    
+                    <td><?php echo ($combinedscore);?>
+          </td>
+                    
+                    
+                    
+                   
+                    
+                    
+            </form>
+            </tr>
+            
+            
+            
+  
+        <?php }} ?>
+    
+ 
+</table>
+        
+        <h2>Potential companies</h2>
+         <table id="myTable" style="width:100%">
+  <tr class="rowhead">
+   
+    
+    <th class="colhead">Company Name</th>
+
+                        <th class="colhead">Contact Person</th>
+                        <th class="colhead">website</th>
+                        <th class="colhead">Description</th>
+                        <th class="colhead">Tier rate</th>
+                        <th class="colhead">notes</th>
+      
+  </tr>
+  
+    
+    
+    
+     <?php 
+  
+         $session14= $_SESSION["stuId"];
+             
+    $sql = "SELECT * FROM  companytierrate";
+$result = $conn->query($sql);
+    while($row = $result->fetch_assoc()) {
+        if($row["companyMinimumRate"]<= $combinedscore && $combinedscore <=$row["companyMaximumRate"]){
+            $row12=$row["companyTier"];
+            
+          $sql1 = "SELECT * FROM  courses";
+$result1 = $conn->query($sql1);
+    while($row = $result1->fetch_assoc()) {
+        if($row["courseName"]== $StuCourse){
+            
+            
+            
+            $courseType=$row["courseType"];
+            
+           
+}}
+          $sql = "SELECT * FROM  company";
+$result = $conn->query($sql);
+    while($row = $result->fetch_assoc()) {
+        if($row["tier_rate"]== $row12 && $row["TypeOfCompany"]==$courseType){   
+            
+            
+         ?>
+            
+            
+            
+            
+            <tr>
+                
+                
+                
+                
+                
+                
+                
+                
+           <td class="colhead1"><?php echo $row["company_name"];?>
+                        </td>
+                        <td class="colhead1">
+                            <?php echo $row["contact_person"];?></td>
+                        <td class="colhead1">
+                            <?php echo $row["website"];?></td>
+                        <td class="colhead1">
+                            <?php echo $row["description"];?></td>
+                        <td class="colhead1">
+                            <?php echo $row["tier_rate"];?></td>
+                        <td class="colhead1">
+                            <?php echo $row["notes"];?></td>
+                        <td><form action="StudentMatch_ap.php" method=post>
+                                <input type="hidden" name="stuId" value="<?php echo $row["s_id"];?>">
+
+
+                                <input class="infoBtn" type="submit"  alt="submit"
+                                    fill="orange" value="enroll" width="17" height="17">
+
+                            </form></td>
+               
+                 
+        
+               
+            
+            </tr>
+            
+            
+            
+  
+        <?php }}}} ?>
+    
+ 
+</table>
+        <?php }
+          else{
+              
+              
+              
+          ?> <P style="color:black;text-align:center;">Please select a student inorder to view their details</P> <?php
+               
+          }
+          
+          
+          ?>
+        
+        
+    </div>
+    
+  </div>
+</div>
+
+ <footer id="sticky-footer" class="footer12">
+    <div class="container text-center">
+     <P>By Pedro Ferraz 6008 and Jayme Schmid 6290</P>
+    </div>
+  </footer>
+
+</body>
+</html>
