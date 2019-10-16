9d0455563c8b613e4160a71f7b3f9b60572a6b62
PedroFerraz2012 <43510971+PedroFerraz2012@users.noreply.github.com>
2019-10-16 19:51:41 +1100

    delete event ok

--- Calendar/delete_event.php
+++ Calendar/delete_event.php
@@ -8,30 +8,39 @@ include_once './connection.php';
 //instantiating data / method post and filer for int
 $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
 
-//preparing query to save in the db, using links for values
+$test= false; 
 
-if (!empty($id)) {
-    //$result_events = "DELETE FROM messages WHERE message_id in ($id)";
-    $result_events = "DELETE FROM messages WHERE message_id = $id"; // myphpadmin is asking confirmation
-    $resultado_events = mysqli_query($conn2, $result_events, true);
-
-    //$update_event = $conn2->prepare($query_event);
-        //getting values to link in the variable
-    //$update_event->bindParam(':id', $dados['id']);
+if( $test==false){
+    echo "flase";
     
-    //checking if DB changed by "mysqli_affected_rows"
-    if (mysqli_affected_rows($conn2)) {
-        $_SESSION['msg'] = "<div class='alert alert-success' role='alert'>Dropped Successfuly<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
+    
+    try{ $sql ="DELETE FROM messages WHERE message_id ='".$id."';" ;
+                
+    
+    if ($conn2->query($sql) === TRUE)
+        {
+            echo "updated";
+            $_SESSION['msg'] = "<div class='alert alert-success' role='alert'>Dropped Successfuly<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
         header("Location: ../Emails.php");
-    } else {
-        $_SESSION['msg'] = "<div class='alert alert-danger' role='alert'>Error at Dropping Event $id<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
+        }
+    else
+        {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+            $_SESSION['msg'] = "<div class='alert alert-danger' role='alert'>Error at Dropping Event $id<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
         header("Location: ../Emails.php");
+        }   
+               
+    echo "updated";
     }
-} else {
-    $_SESSION['msg'] = "<div class='alert alert-danger' role='alert'>Error at Dropping Event (ID empty) <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
-    header("Location: ../Emails.php");
-}
 
+catch(PDOException $e)
+    {
+        echo $sql . "<br>" . $e->getMessage();
+    }
+        $_SESSION['msg'] = "<div class='alert alert-danger' role='alert'>PDOException $e Check Dropping Event $id<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
+            echo $_SESSION['msg'];    
+    }
+header('Location: ../Emails.php');
+$conn2->close();
 
-//header('Content-Type: application/json');
-//echo json_encode($retorna);
+?>
